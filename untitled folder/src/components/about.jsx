47906ee8afecc622d9ac79ebdba91f6e44d7caad
DoveCoder <76623779+DoveCoder@users.noreply.github.com>
2021-11-08 23:37:52 -0800

    Add files via upload

---
+++ untitled folder/src/components/about.jsx
@@ -0,0 +1,14 @@
+import './about.css'
+
+const About = () => {
+    return(
+        <div>
+            <h1>About me:</h1>
+            <h5>Jimmy Newtron</h5>
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi expedita praesentium voluptatibus dolore sapiente aliquid odit officiis illo esse pariatur, cupiditate nobis! Nesciunt neque, nulla placeat consectetur ratione ducimus rem.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ untitled folder/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import './footer.css'
+
+
+function Footer()
+{
+    return (
+        <div className="footSection">
+            <h5>A great place to be! <em>2021</em></h5>
+            <h6>Julian Smith</h6>
+        </div>
+    );
+}
+
+export default Footer;
